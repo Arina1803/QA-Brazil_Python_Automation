d97a61063dd7c46af475baed7e98086c8bc843a4
PC-CASA <arinalfernandes@gmail.com>
2025-06-01 23:18:30 -0300

    repositor done

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = "https://cnt-3313d65f-b859-4fcd-885c-3cd2a8c555eb.containerhub.tripleten-services.com?lng=pt"
+
+ADDRESS_FROM = "East 2nd Street, 601"
+ADDRESS_TO = "1300 1st St"
+PHONE_NUMBER = "+1 123 123 12 12"
+CARD_NUMBER = "1234 5678 9100"
+CARD_CODE = "1111"
+MESSAGE_FOR_DRIVER = "Pare no bar de sucos"
